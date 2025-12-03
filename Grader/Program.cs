9f6645c904ecc83d70aeccb8d1ab4d25c30abed3
Kyle <kyled616@gmail.com>
2025-12-02 22:37:41 -0500

    changes

---
+++ Grader/Program.cs
@@ -0,0 +1,19 @@
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+List<int> sophiasScores = new List<int> {93, 87, 98, 95, 100};
+List<int> nicolasScores = new List<int> {80, 83, 82, 88, 85}
+List<int> zahirahScores = new List<int> {}
+List<int> jeongScores = new List<int> {}
+
+for (int sophiaSum = 0, i )score in sophiasScores += score
+int sophiaSum = 1;
+int nicolasSum = 0;
+int zahirahSum = 0;
+int jeongSum = 0;
+
+
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahirah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
